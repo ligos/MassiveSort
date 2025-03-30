4c13eadcff363754a36cb580d6170f337f8b0fff
Murray Grant <github@ligos.net>
2025-03-30 17:36:23 +1100

    Update build script to actually build, and package

--- build.cmd
+++ build.cmd
@@ -1,3 +1,12 @@
+rem Rebuild
+dotnet clean -c Release
+dotnet build -c Release
+
+rem Publish for .NET 8.0
+dotnet publish MassiveSort.csproj -c Release -f net8.0
+
+rem Build ZIP file for .NET 8.0
 del /q MassiveSort.zip
-zip.exe -9jr MassiveSort.zip bin\Release\net4.8\*.* 
+cd bin\Release\net8.0\publish
+..\..\..\..\zip.exe -9r ..\..\..\..\MassiveSort.zip *.*
 pause
