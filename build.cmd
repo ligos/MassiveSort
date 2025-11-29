fd13f5b843f6a20c9cc2f88fa9613932c3ab6df2
Murray Grant <github@ligos.net>
2025-11-29 17:58:45 +1100

    Update build.cmd to target net10.0

--- build.cmd
+++ build.cmd
@@ -2,11 +2,11 @@ rem Rebuild
 dotnet clean -c Release
 dotnet build -c Release
 
-rem Publish for .NET 8.0
-dotnet publish MassiveSort.csproj -c Release -f net8.0
+rem Publish for .NET 10.0
+dotnet publish MassiveSort.csproj -c Release -f net10.0
 
-rem Build ZIP file for .NET 8.0
+rem Build ZIP file for .NET 10.0
 del /q MassiveSort.zip
-cd bin\Release\net8.0\publish
+cd bin\Release\net10.0\publish
 ..\..\..\..\zip.exe -9r ..\..\..\..\MassiveSort.zip *.*
 pause
